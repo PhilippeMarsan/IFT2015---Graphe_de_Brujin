695308e7c39a90147c30b83c0abbc44f31498bf0
Philippe Marsan <philippe.marsan-loyer@umontreal.ca>
2018-12-20 12:41:08 -0500

    debut

--- DeBrujinGraph.py
+++ DeBrujinGraph.py
@@ -1,4 +1,5 @@
 class DeBrujinGraph:
+
     def __init__(self, nodes: Iterable[str], k=21):
         pass # initialise la structure de données
 
@@ -25,3 +26,59 @@ class DeBrujinGraph:
 
     def successors(self, N: str) -> Iterable[str]:
         pass # retourne tous les successeurs du noeud N
+
+
+
+'''
+Implementation de hasMap avec sondage lineaire
+(pour favoriser la rechercher lorsque l'on veut trouver les predecesseur et successeur)
+TODO changer l'approche de division pour MAD
+'''
+class HashMapDebrujin:
+
+    '''
+    initialise le Hashmap en preparant le tableau
+    et en sauvegardant la valeur
+    '''
+    def __init__(self, cap=100):
+        self._data  = [null] * cap #table des valeurs
+        self._cap = cap #capacite de la table
+        self._n = 0 #nbr d'element dans la table
+        # self.p = #TODO pour implementer avec nombre premier
+
+    '''
+    Fonction pour obtennir une clefs si rien
+    n'est passer a setitem. Specifique au probleme passe en parametre
+    '''
+    def _getKey(self, value):
+        pass
+
+    def _hashFunction(self, value):
+        pass
+
+    def __len__(self):
+        return self._cap
+
+    def __getitem__:
+        pass
+
+    def __setitem__(self, k=null , v):
+        if (k == null):
+            k = self._getKey(v)
+
+        pass 
+
+    def __delitem__(self, k):
+        pass
+
+    def _resize(self):
+        pass
+
+
+    '''
+    renvoie les clefs du tableau
+    '''
+    def __iter__:
+        for i in range(len(self._data)):
+            if (not(i == null) and not(i == _AVAIL)):
+                yield self.data[i]
